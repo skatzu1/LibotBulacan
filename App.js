ef36dc134d23535901e7da4fb48157ca8a725616
Your Name <charlesgumila789@gmail.com>
2026-01-21 09:40:19 +0800

    asd

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>asasdd</Text>
+      <Text>ano na</Text>
       <StatusBar style="auto" />
     </View>
   );
