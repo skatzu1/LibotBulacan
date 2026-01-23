f08a4d1e2f0cae9c8469248e3e5a2d062ba8d211
Your Name <charlesgumila789@gmail.com>
2026-01-23 11:58:04 +0800

    asdasd

--- Screens/Login.js
+++ Screens/Login.js
@@ -1,36 +1,74 @@
-import { View, StyleSheet, Button, TextInput } from "react-native";
+import { View, StyleSheet, TouchableOpacity, TextInput, Text } from "react-native";
 
 export default function Login() {
   return (
-    <View style={styles.container}>
-      <TextInput
-        placeholder="Username"
-        style={styles.input}
-      />
+    <View style={styles.screen}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Sign in</Text>
+        <Text style={styles.subtitle}>
+          Please sign in to continue
+        </Text>
+      </View>
 
-      <TextInput
-        placeholder="Password"
-        secureTextEntry
-        style={styles.input}
-      />
-
-      <Button title="Login" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder="Username" />
+        <TextInput style={styles.input} placeholder="Password" secureTextEntry />
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+    screen: {
+    backgroundColor: "#f7cfc9",
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-  },
-  input: {
-    height: 40,
-    width: 200,
-    borderColor: "gray",
-    borderWidth: 1,
+    padding: 20,
+    },
+    titleContainer: {
+    alignItems: "center", 
+    },
+    title: {
+    fontSize: 25,
+    fontWeight: "700",
+    textAlign: "center",
+    },
+    subtitle: {
+    fontSize: 18,
+    textAlign: "center",
+    color: "#444",
+    marginTop: 10,
+    },
+    inputContainer: {
+    width: "100%",
+    marginBottom: 20,
+    },
+    input: {
+    backgroundColor: "#fff",
+    padding: 15,
+    borderRadius: 10,
     marginBottom: 10,
-    paddingHorizontal: 10,
+    width: "100%",
+    },
+    buttonContainer: {
+    width: "100%",
+    },
+    button: {
+    backgroundColor: "#444",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+    width: "100%",
+    },
+    buttonText: {
+    color: "#fff",
+    fontWeight: "700",
+    fontSize: 18,
   },
 });
