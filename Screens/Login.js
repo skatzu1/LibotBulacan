6a5376843b474acd9359772cdd808b0793923073
Your Name <charlesgumila789@gmail.com>
2026-01-24 08:22:20 +0800

    Login Changes

--- Screens/Login.js
+++ Screens/Login.js
@@ -1,6 +1,11 @@
 import { View, StyleSheet, TouchableOpacity, TextInput, Text } from "react-native";
+import { useState } from "react";
+import CheckBox, { Checkbox } from "expo-checkbox";
 
 export default function Login() {
+  const [isChecked, setChecked] = useState(false);
+  const handleForgotPassword = () => { alert("nigga"); }
+  const [PasswordVisible, setPasswordVisible] = useState(false);
   return (
     <View style={styles.screen}>
       <View style={styles.titleContainer}>
@@ -12,63 +17,111 @@ export default function Login() {
 
       <View style={styles.inputContainer}>
         <TextInput style={styles.input} placeholder="Username" />
-        <TextInput style={styles.input} placeholder="Password" secureTextEntry />
+        <TextInput style={styles.input} placeholder="Password" 
+        secureTextEntry={PasswordVisible} />
       </View>
+
+      <View style={styles.showhideContainer}>
+        <TouchableOpacity onPress={() => setPasswordVisible(!PasswordVisible)}>
+          <Text style={styles.eyeButton}>{PasswordVisible ? "Show" : "Hide"} Password</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.checkboxContainer}>
+        <View style={styles.rememberRow}>
+          <CheckBox value={isChecked} onValueChange={setChecked}
+          color={isChecked ? "#f7cfc9" : "#ffffff"}
+          marginLeft={25}/>
+          <Text style={styles.rememberMe}>Remember me</Text>
+        </View>
+        <TouchableOpacity onPress={handleForgotPassword}>
+          <Text style={styles.forgotPassword}>Forgot Password?</Text>
+        </TouchableOpacity>
+      </View>
+
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
       </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
     screen: {
-    backgroundColor: "#f7cfc9",
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 20,
+      backgroundColor: "#f7cfc9",
+      flex: 1,
+      padding: 20,
+      paddingTop: 80,
     },
     titleContainer: {
-    alignItems: "center", 
+      alignItems: "center", 
+      marginBottom: 80,
+      marginTop: 60,
     },
     title: {
-    fontSize: 25,
-    fontWeight: "700",
-    textAlign: "center",
+      fontSize: 25,
+      fontWeight: "700",
+      textAlign: "center",
     },
     subtitle: {
-    fontSize: 18,
-    textAlign: "center",
-    color: "#444",
-    marginTop: 10,
+      fontSize: 18,
+      textAlign: "center",
+      color: "#444",
+      marginTop: 10,
     },
     inputContainer: {
-    width: "100%",
-    marginBottom: 20,
+      width: "95%",
+      marginBottom: -5,
+      alignSelf: "center",
+      gap: 20,
     },
     input: {
-    backgroundColor: "#fff",
-    padding: 15,
-    borderRadius: 10,
-    marginBottom: 10,
-    width: "100%",
+      backgroundColor: "#fff",
+      padding: 15,
+      borderRadius: 10,
+      marginBottom: 10,
+      width: "100%",
     },
     buttonContainer: {
-    width: "100%",
+      width: "100%",
+      alignItems: "center",
     },
     button: {
-    backgroundColor: "#444",
-    padding: 15,
-    borderRadius: 10,
-    alignItems: "center",
-    width: "100%",
+      backgroundColor: "#6b4b45",
+      padding: 15,
+      borderRadius: 10,
+      alignItems: "center",
+      width: "80%",
     },
     buttonText: {
-    color: "#fff",
-    fontWeight: "700",
-    fontSize: 18,
-  },
+      color: "#fff",
+      fontWeight: "700",
+      fontSize: 18,
+    },
+    checkboxContainer: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      width: "100%",
+      marginBottom: 30,
+    },
+    rememberRow: {
+      flexDirection: "row",
+      alignItems: "center",
+    },
+    rememberMe: {
+      marginLeft: 8,
+      fontSize: 16,
+    },
+    forgotPassword: {
+      fontSize: 13,
+      marginRight: 30,
+    },
+    showhideContainer: {
+      marginLeft: 25,
+      marginBottom: 15,
+    }
 });
