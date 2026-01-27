df2a9401bf0e6c2f8434eaf37a77399debe2d0e6
komiigurii <jasperdwayne22@gmail.com>
2026-01-27 23:13:38 +0800

    host

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getUsers, createUser } = require('../controllers/userController');
-
-// Example endpoints
-router.get('/', getUsers);
-router.post('/', createUser);
-
-module.exports = router;
