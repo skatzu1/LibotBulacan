716627a495b283e9695e4bda9e2292a7354d3b25
Your Name <charlesgumila789@gmail.com>
2026-01-27 20:53:09 +0800

    Server Changes

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
