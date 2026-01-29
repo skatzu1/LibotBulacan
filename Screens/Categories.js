ef62a5e08ad8c6ba473298e02da6503807ef29a5
Your Name <charlesgumila789@gmail.com>
2026-01-29 08:16:43 +0800

    Categories UPD

--- Screens/Categories.js
+++ Screens/Categories.js
@@ -6,23 +6,45 @@ export default function Categories(){
     const navigation = useNavigation();
     
     return(
-        <View style={{flex:1, justifyContent:'center', alignItems:'center', backgroundColor: "#f7cfc9"}}>
-            <View style={styles.categoriesHeader}>
-                <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-                    <Text>asdasdasd</Text>
+        <View style={{flex:1, justifyContent:'flex-start', alignItems:'center', backgroundColor: "#f7cfc9", paddingTop: 50, flex: 1}}>
+            <View style={styles.header}>
+                <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
+                    <Feather name="menu" size={28} />
                 </TouchableOpacity>
+                <Text style={styles.headerTitle}>Categories</Text>
+                <Text>asd</Text>
+            </View>
+
+            <View style={styles.centeredText}>
+                <Text style={styles.subtitle}>Explore and Have fun!</Text>
             </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    categoriesHeader: {
+    header: {
         width: "90%",
         flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center",
         marginBottom: 20,
         alignSelf: "center",
-    }
+    },
+
+    headerTitle: {
+        fontSize: 22,
+        fontWeight: "200",
+    },
+
+    centeredText: {
+        width: "100%",
+        marginBottom: 10,
+        alignItems: "center",
+    },
+
+    subtitle: {
+        fontSize: 18,
+        fontWeight: "400",
+    },
 });
