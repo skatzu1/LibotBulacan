fb025b798bf90d8350c2d8f4dc2a455e66bf71f2
komiigurii <jasperdwayne22@gmail.com>
2026-02-05 21:27:10 +0800

    asd

--- Screens/ARScreen.js
+++ Screens/ARScreen.js
@@ -152,7 +152,11 @@ class ARScene extends Component {
   };
 
   render() {
-    const { arPositions, collectedObjects, onCollect } = this.props;
+  const {
+    arPositions = [],
+    collectedObjects = [],
+    onCollect = () => {},
+  } = this.props.sceneNavigator?.viroAppProps || {};
 
     return (
       <ViroARScene onTrackingUpdated={this.onTrackingUpdated}>
